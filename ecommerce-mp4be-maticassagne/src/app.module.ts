684852e77c8f230e3d3e29e8d5159326ffa57857
MatiCassagne <matiascassagne@gmail.com>
2025-07-30 12:59:02 -0300

    Agregado HW1 y HW 2

---
+++ ecommerce-mp4be-maticassagne/src/app.module.ts
@@ -0,0 +1,18 @@
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+
+@Module({
+  imports: [ProductsModule, UsersModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
